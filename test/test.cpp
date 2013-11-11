f779f459fb2e9663d8177741db869b644b2f8e6d
Mike Ando <d.mike.ando@gmail.com>
2013-11-11 19:18:51 +0000

    Removed some test files

--- test/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <iostream>
-extern "C" {
-  void runalgo() {
-    std::string haystack("ABACAB is it everywhere!");
-    std::string needle1 ("ACAB");
-    std::string needle2 ("not ABA");
-  }
-} // extern "C"
--- test/test.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef TEST_H 
-#define TEST_H 
-extern "C" { 
-  void runalgo();
-}
-#endif // TEST_H
